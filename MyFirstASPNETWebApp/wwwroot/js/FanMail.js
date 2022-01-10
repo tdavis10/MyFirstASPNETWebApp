4f839878293201dd42bbf7bb7e37c05fbb9d9987
tdavis10 <tanner.davis002@gmail.com>
2022-01-09 21:36:08 -0700

    Add project files.

---
+++ MyFirstASPNETWebApp/wwwroot/js/FanMail.js
@@ -0,0 +1,13 @@
+﻿document.getElementById("btnSend").addEventListener("click", function () {
+    alert("From: " + document.getElementById("txtFrom").value + ", Subject: "+ document.getElementById("txtSubject").value + ", Message: " + document.getElementById("txtMessage").value);
+})
+
+
+
+$("#btnSend").click(function () {
+    alert("From: " + $("#txtFrom").val() +
+        ", Subject: " + $("#txtSubject").val() +
+        ", Message: " + $("#txtMessage").val());
+
+    $("#picGanderson").fadeToggle("slow");
+})
